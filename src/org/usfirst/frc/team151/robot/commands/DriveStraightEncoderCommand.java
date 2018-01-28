a9559f08d98dc8c8d6efe110d1ec90e39bf94f76
julia <julia@DESKTOP-ULBIMQH>
2018-01-28 11:22:56 -0500

    Forgot to commit autonomous command with encoders

---
+++ src/org/usfirst/frc/team151/robot/commands/DriveStraightEncoderCommand.java
@@ -0,0 +1,49 @@
+package org.usfirst.frc.team151.robot.commands;
+
+import org.usfirst.frc.team151.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class DriveStraightEncoderCommand extends Command {
+	
+	private boolean isFinished = false;
+	private double distance;
+
+    public DriveStraightEncoderCommand(double initDistance) {
+    	distance = initDistance;
+        requires(Robot.TANK_DRIVE_SUBSYSTEM);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	if (Robot.TANK_DRIVE_SUBSYSTEM.getEncoder() < distance) {
+    		Robot.TANK_DRIVE_SUBSYSTEM.drive(0.25, 0.25);
+    		System.out.println("Left Encoder Distance: " + Robot.TANK_DRIVE_SUBSYSTEM.leftEnc.getDistance());
+    		System.out.println("Left Encoder Count: " + Robot.TANK_DRIVE_SUBSYSTEM.leftEnc.get());
+    		System.out.println("Right Encoder Distance: " + Robot.TANK_DRIVE_SUBSYSTEM.rightEnc.getDistance());
+    		System.out.println("Right Encoder Count: " + Robot.TANK_DRIVE_SUBSYSTEM.rightEnc.get());
+    	}
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	for ()
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
